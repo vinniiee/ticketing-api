696f05fae012364d0dc7d5820d7dd81607af4d04
vinniiee <vs28245@gmail.com>
2023-02-26 00:02:36 +0530

    add cors header manually

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import "express-async-errors";
@@ -20,12 +20,18 @@ app.use(
 
 
 app.use(json());
-app.use(cors({
-  origin:["https://glittery-melba-88ab24.netlify.app/","https://ticketing-client-wheat.vercel.app/","https://ticketing-client*"],
-  allowedHeaders: [ "Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization", "x-xsrf-token","Set-Cookie" ],
-  credentials:true,
-  methods:['GET','POST','DELETE','UPDATE','PUT','PATCH']
-}));
+// app.use(cors({
+//   origin:["https://glittery-melba-88ab24.netlify.app/","https://ticketing-client-wheat.vercel.app/","https://ticketing-client*"],
+//   allowedHeaders: [ "Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization", "x-xsrf-token","Set-Cookie" ],
+//   credentials:true,
+//   methods:['GET','POST','DELETE','UPDATE','PUT','PATCH']
+// }));
+
+app.use((req:Request,res:Response,next:NextFunction)=>{
+    res.header("Access-Control-Allow-Origin","https://glittery-melba-88ab24.netlify.app/events");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.all("/auth/*", authRouter);
 // app.all("*",(req,res)=>{
 //     res.send("Hello!")
