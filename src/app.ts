7fafe0cc02a981ec2f97793a0bd073ff323f0cad
vinniiee <vs28245@gmail.com>
2023-02-25 23:43:34 +0530

    update cors origin

--- src/app.ts
+++ src/app.ts
@@ -17,10 +17,12 @@ app.use(
     // keys: ['key1', 'key2']
   })
 );
+
+
 app.use(json());
 app.use(cors({
-  origin:"*",
-  allowedHeaders: ['*'],
+  origin:["https://glittery-melba-88ab24.netlify.app/","https://ticketing-client-wheat.vercel.app/","https://ticketing-client*"],
+  allowedHeaders: [ "Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization", "x-xsrf-token","Set-Cookie" ],
   credentials:true,
   methods:['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
