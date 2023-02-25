c801d7f3f00515bb9f2295e8ec4b1cff36863d58
vinniiee <vs28245@gmail.com>
2023-02-26 01:27:52 +0530

    add custom cors middleware

--- src/app.ts
+++ src/app.ts
@@ -20,12 +20,26 @@ app.use(
 
 
 app.use(json());
-app.use(cors({
-  origin:["http://localhost:3001","https://glittery-melba-88ab24.netlify.app/","https://ticketing-client-wheat.vercel.app/","https://ticketing-client"],
-  allowedHeaders: [ "Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization", "x-xsrf-token","Set-Cookie" ],
-  credentials:true,
-  methods:['GET','POST','DELETE','UPDATE','PUT','PATCH']}
-));
+
+app.use(function(req:Request, res:Response, next:NextFunction) {
+  // res.header("Access-Control-Allow-Origin", "*");
+  const allowedOrigins = ["http://localhost:3001","https://glittery-melba-88ab24.netlify.app/","https://ticketing-client-wheat.vercel.app/","https://ticketing-client"];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin!)){
+       res.setHeader('Access-Control-Allow-Origin', origin!);
+  }
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-credentials", 'true');
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+  next();
+});
+
+// app.use(cors({
+//   origin:["http://localhost:3001","https://glittery-melba-88ab24.netlify.app/","https://ticketing-client-wheat.vercel.app/","https://ticketing-client"],
+//   allowedHeaders: [ "Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization", "x-xsrf-token","Set-Cookie" ],
+//   credentials:true,
+//   methods:['GET','POST','DELETE','UPDATE','PUT','PATCH']}
+// ));
 
 // app.use((req:Request,res:Response,next:NextFunction)=>{
 //     res.header("Access-Control-Allow-Origin","https://glittery-melba-88ab24.netlify.app");
