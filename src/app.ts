455cd5778d02555e5bbf285f33f5e47ba7707376
vinniiee <vs28245@gmail.com>
2023-03-03 02:39:11 +0530

    set deafult cors

--- src/app.ts
+++ src/app.ts
@@ -17,22 +17,22 @@ app.use(
     // keys: ['key1', 'key2']
   })
 );
-
+app.use(cors());
 
 app.use(json());
 
-app.use(function(req:Request, res:Response, next:NextFunction) {
-  // res.header("Access-Control-Allow-Origin", "*");
-  const allowedOrigins = ["http://localhost:3001","https://glittery-melba-88ab24.netlify.app/","https://ticketing-client-wheat.vercel.app/","https://ticketing-client"];
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin!)){
-       res.setHeader('Access-Control-Allow-Origin', origin!);
-  }
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-credentials", 'true');
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-  next();
-});
+// app.use(function(req:Request, res:Response, next:NextFunction) {
+//   // res.header("Access-Control-Allow-Origin", "*");
+//   const allowedOrigins = ["http://localhost:3001","https://glittery-melba-88ab24.netlify.app/","https://ticketing-client-wheat.vercel.app/","https://ticketing-client"];
+//   const origin = req.headers.origin;
+//   if (allowedOrigins.includes(origin!)){
+//        res.setHeader('Access-Control-Allow-Origin', origin!);
+//   }
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   res.header("Access-Control-Allow-credentials", 'true');
+//   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
+//   next();
+// });
 
 // app.use(cors({
 //   origin:["http://localhost:3001","https://glittery-melba-88ab24.netlify.app/","https://ticketing-client-wheat.vercel.app/","https://ticketing-client"],
