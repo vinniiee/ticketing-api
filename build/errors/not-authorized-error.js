5324e6f00741f2c91f7cb522306920330155a3d2
vinniiee <vs28245@gmail.com>
2023-03-03 02:05:31 +0530

    update port

--- build/errors/not-authorized-error.js
+++ build/errors/not-authorized-error.js
@@ -3,12 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.NotAuthorizedError = void 0;
 const custom_error_1 = require("./custom-error");
 class NotAuthorizedError extends custom_error_1.CustomError {
+    serializeErrors() { return [{ message: "Not Authorized" }]; }
     // throw new Error("Method not implemented.");
     constructor() {
         super("Not Authorized");
         this.statusCode = 401;
         Object.setPrototypeOf(this, NotAuthorizedError.prototype);
     }
-    serializeErrors() { return [{ message: "Not Authorized" }]; }
 }
 exports.NotAuthorizedError = NotAuthorizedError;
--- src/index.ts
+++ src/index.ts
@@ -23,7 +23,7 @@ const start = async ()=>{
     }
 
 
-    app.listen(3000, () => {
+    app.listen(process.env.PORT || 3000, () => {
         console.log("Listening on port 3000...");
       });
 
